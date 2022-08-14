1a03194191e51d339b756e2e65793b4753f6c2e7
ฟาตอนียะห์ หะยีมายอ <najwa.26252@gmail.com>
2022-08-14 09:02:23 +0700

    part 7

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
-import { Button } from 'reactstrap';
+
 
 const word = "Fatoniyah";
 function App() {
   return (
     <div>
-      <Button outline color="success">my name is ... ?</Button>{' '}
+      <h1>ให้ทายฉันชื่ออะไร</h1>
       <WordCard value={word}/>
       <p> คำใบ้ : ชื่อของฉันมี 4 พยางค์ ขึ้นต้นด้วยตัวย F ลงท้ายด้วย H </p>
       
